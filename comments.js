27c07353bb16d05b3f635c26ac0df46f339113b0
Abel Terefe <abel.terefe@trademarknow.com>
2021-08-25 17:21:42 +0300

    Add models

---
+++ comments.js
@@ -0,0 +1,9 @@
+class Comment {
+  constructor({ title, content, author, created, modified }) {
+    this.title = title;
+    this.content = content;
+    this.author = author;
+    this.created = created;
+    this.modified = modified;
+  }
+}
